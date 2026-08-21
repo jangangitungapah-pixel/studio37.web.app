84029298cfdd3179d637218242a2a7dd2889eda6
jangangitungapah-pixel <jangangitungapah@gmail.com>
2026-08-21 20:49:01 +0700

    feat(phase-1c): wire global toast provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './app/App.jsx';
+import { ToastProvider } from './components/feedback/ToastProvider.jsx';
 import './styles/index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </StrictMode>,
 );
