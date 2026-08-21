47bf7eac1de748ad0bb65618f3bc2c083872062f
jangangitungapah-pixel <jangangitungapah@gmail.com>
2026-08-21 19:12:44 +0700

    fix(phase-0): recognize JSX variable usage in ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
@@ -24,11 +25,13 @@ export default [
       },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'react/jsx-uses-vars': 'error',
       'react-refresh/only-export-components': ['error', { allowConstantExport: true }],
     },
   },
